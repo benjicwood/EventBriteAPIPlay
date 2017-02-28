b80912cca7ddfb0851d7e8e38951d28c0bf0c216
benjicwood <bencharlwood@gmail.com>
2017-02-28 16:45:10 +0000

    add testing for helper function

--- src/helpers/htmlparser.js
+++ src/helpers/htmlparser.js
@@ -1,12 +1,12 @@
-/** @jsx React.DOM */
-
-var Main = React.createClass({
-    getInitialState: function() {
-        return {output: 'hello<br />world<br />'};
-      },
-    render: function() {
-        return (
-            <div dangerouslySetInnerHTML={{__html: this.state.output}} />
-        );
-        }
-});
+// /** @jsx React.DOM */
+//
+// var Main = React.createClass({
+//     getInitialState: function () {
+//         return {output: 'hello<br />world<br />'};
+//       },
+//     render: function () {
+//         return (
+//             <div dangerouslySetInnerHTML={{__html: this.state.output}} />
+//         );
+//         }
+// });
