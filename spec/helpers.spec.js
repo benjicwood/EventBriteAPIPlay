d1fd94bdc449f12ec22339181406bb30b94e0fc3
benjicwood <bencharlwood@gmail.com>
2017-02-28 23:04:10 +0000

    add tests for the helper function dateformat

--- spec/helpers.spec.js
+++ spec/helpers.spec.js
@@ -10,5 +10,17 @@ describe('helper functions', function () {
     it('takes one argument', function () {
       expect(dateformat.length).to.equal(1);
     });
+    it('returns a string format using .toString()', function () {
+      var apidate = '2015-11-08T01:00:00';
+      expect(dateformat(apidate)).to.be.a('string');
+    });
+    it('formats the date into a more readable format', function () {
+      var apidate = '2015-11-08T01:00:00';
+      expect(dateformat(apidate)).to.equal('Sun Nov 08 2015 01:00:00');
+    });
+    it('removes the timezone (GMT+0000 (GMT)) from the formatted string by slicing the string', function () {
+      var apidate = '2015-11-08T01:00:00';
+      expect(dateformat(apidate).length).to.equal(24);
+    });
   });
 });
