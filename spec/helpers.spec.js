280e8ad0ab8d89c9cd99d33afc6590fee3bca766
benjicwood <bencharlwood@gmail.com>
2017-02-28 23:04:24 +0000

    add tests for the helper function dateformat

--- spec/helpers.spec.js
+++ spec/helpers.spec.js
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import dateformat from '../src/helpers/dateformat';
 
 describe('helper functions', function () {
-  describe('dataformat', function () {
+  describe('dateformat', function () {
     it('is a function', function () {
       expect(dateformat).to.be.a('function');
     });
