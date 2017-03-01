dc81b1217e3b6a9cc098f91d93b2868a5a57558a
benjicwood <bencharlwood@gmail.com>
2017-03-01 22:09:26 +0000

    minor linting fixes

--- src/components/Event.js
+++ src/components/Event.js
@@ -10,22 +10,22 @@ export default class Event extends Component {
     return (
       <div style={styles.background}>
         <div className='columns'>
-        <div className='card' style={styles.card}>
-          <div className='card-content'>
-            <div className='media'>
-              <div className='media-left'>
-                <figure className='image is-128x128'>
-                  <img src={this.props.image} style={styles.image} alt='Image' />
-                </figure>
-              </div>
-              <div className='media-content'>
-                <p className='title is-5' style={styles.font}>{this.props.name}</p>
-                <p className='subtitle is-6'>{date(this.props.startdate)}</p>
-                <ModalView html={this.props.html} /><a className='button is-hovered' href={this.props.url}>Go To Event</a>
+          <div className='card' style={styles.card}>
+            <div className='card-content'>
+              <div className='media'>
+                <div className='media-left'>
+                  <figure className='image is-128x128'>
+                    <img src={this.props.image} style={styles.image} alt='Image' />
+                  </figure>
+                </div>
+                <div className='media-content'>
+                  <p className='title is-5' style={styles.font}>{this.props.name}</p>
+                  <p className='subtitle is-6'>{date(this.props.startdate)}</p>
+                  <ModalView html={this.props.html} /><a className='button is-hovered' href={this.props.url}>Go To Event</a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
         </div>
       </div>
     );
