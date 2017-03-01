9eccfc24424fa471b5e7fa80817304c164001028
benjicwood <bencharlwood@gmail.com>
2017-03-01 17:22:15 +0000

    add heroku

--- server.babel.js
+++ server.babel.js
@@ -1,7 +1,12 @@
+import cool from 'cool-ascii-faces';
 import express from 'express';
 
 const app = express();
 
 app.use('/', express.static('public'));
+
+app.get('/cool', function (request, response) {
+  response.send(cool());
+});
 
 app.listen(process.env.PORT || 3000);
